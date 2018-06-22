2cb21aea41f451a11a7c214ee1c5c4a54788a1b2
Anwesh43 <anweshthecool0@gmail.com>
2018-06-22 09:37:21 +0530

    adding LinkedTriWedgeView class

--- linkedtriwedgeview/src/main/java/com/anwesh/uiprojects/linkedtriwedgeview/LinkedTriWedgeView.kt
+++ linkedtriwedgeview/src/main/java/com/anwesh/uiprojects/linkedtriwedgeview/LinkedTriWedgeView.kt
@@ -3,3 +3,25 @@ package com.anwesh.uiprojects.linkedtriwedgeview
 /**
  * Created by anweshmishra on 22/06/18.
  */
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.*
+import android.content.Context
+
+class LinkedTriWedgeView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                
+            }
+        }
+        return true
+    }
+}
