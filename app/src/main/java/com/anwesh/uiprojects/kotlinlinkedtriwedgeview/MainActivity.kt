c969949ae76e9b67696bffb72db33c2f4a555160
Anwesh43 <anweshthecool0@gmail.com>
2018-06-22 10:54:17 +0530

    adding LinkedTriWedgeView in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedtriwedgeview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedtriwedgeview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedtriwedgeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedtriwedgeview.LinkedTriWedgeView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LinkedTriWedgeView.create(this)
     }
 }
